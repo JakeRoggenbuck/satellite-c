490636de6d0ddd41709033d460933f9e08baf662
Jake <jakeroggenbuck2@gmail.com>
2023-03-12 18:05:00 -0700

    Add sleeping

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
-#include "init_task.h"
 #include "health_check_task.h"
+#include "init_task.h"
 #include "task.h"
 #include "task_list.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-void create_tasks(struct TaskList *task_list) {
+#define SLEEP_TIME 10
+
+void create_first_startup_tasks(struct TaskList *task_list) {
     struct Task *init = get_init_task();
     add(task_list, init);
 
@@ -13,15 +16,31 @@ void create_tasks(struct TaskList *task_list) {
     add(task_list, health_check);
 }
 
-int main() {
-    struct TaskList *task_list = build_tasklist();
-    create_tasks(task_list);
+void create_wake_up_tasks(struct TaskList *task_list) {
+    struct Task *init = get_init_task();
+    add(task_list, init);
+}
 
+void run_tasks(struct TaskList *task_list) {
     struct Task *current;
     while (!empty(task_list)) {
         current = pop(task_list);
         run(current);
     }
+}
 
+int main() {
+    struct TaskList *task_list = build_tasklist();
+    create_first_startup_tasks(task_list);
+    run_tasks(task_list);
+
+    while (1) {
+        create_wake_up_tasks(task_list);
+        run_tasks(task_list);
+
+        printf("INFO: Sleeping...\n");
+        sleep(SLEEP_TIME);
+        printf("INFO: Woke up...\n");
+    }
     return 0;
 }
